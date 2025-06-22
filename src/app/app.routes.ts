850ba8a132e9d027fc8cc410afc663bc18e6f7a5
Duvan Caicedo <duvamendi2@hotmail.com>
2025-06-22 00:30:08 -0500

    add layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,16 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { CsvPreview } from './pages/csv-preview/csv-preview';
+import { Layout } from './layout/layout';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: Home,
-    },
-    {
-        path: 'csv-preview',
-        component: CsvPreview,
-       
-    },
-    {
-        path: '**',
-        redirectTo: ''
-    },
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: '', component: Home },
+      { path: 'csv-preview', component: CsvPreview },
+    ],
+  },
+  { path: '**', redirectTo: '' },
 ];
--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Footer } from './components/footer/footer';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [RouterOutlet, Footer, Header],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
