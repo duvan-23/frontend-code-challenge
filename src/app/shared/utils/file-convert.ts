345575a89dba009b2cfe0efdb88f4df004fa648b
Duvan Caicedo <duvamendi2@hotmail.com>
2025-06-23 15:13:00 -0500

    add file helper functions for file encoding

---
+++ src/app/shared/utils/file-convert.ts
@@ -0,0 +1,12 @@
+export class FileConvert{
+
+  static fileToBase64(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => resolve(reader.result as string);
+      reader.onerror = reject;
+      reader.readAsDataURL(file);
+    });
+  }
+
+}
