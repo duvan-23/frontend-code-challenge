9c6b04861476e085014b4c2d3dbdee68ac753e94
Duvan Caicedo <duvamendi2@hotmail.com>
2025-06-23 15:16:33 -0500

    add service session storage

---
+++ src/app/shared/services/session-storage.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionStorage {
+
+  setItem<T>(key: string, value: T): void {
+    try {
+      sessionStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error(`Error saving to sessionStorage: ${key}`, error);
+    }
+  }
+
+  getItem<T>(key: string): T | null {
+    try {
+      const item = sessionStorage.getItem(key);
+      return item ? (JSON.parse(item) as T) : null;
+    } catch (error) {
+      console.error(`Error reading from sessionStorage: ${key}`, error);
+      return null;
+    }
+  }
+
+  removeItem(key: string) {
+    sessionStorage.removeItem(key);
+  }
+
+  clear() {
+    sessionStorage.clear();
+  }
+}
