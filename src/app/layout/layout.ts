3a49a73aa7b89253824d26783c4be7e77043bb7b
Duvan Caicedo <duvamendi2@hotmail.com>
2025-06-24 23:21:34 -0500

    delete footer importation

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, Footer, Header],
+  imports: [RouterOutlet, Header],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
