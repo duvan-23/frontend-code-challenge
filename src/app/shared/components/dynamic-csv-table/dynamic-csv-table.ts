80ceb4e350223d76ebb5876c8bb6df0b8ae17f97
Duvan Caicedo <duvamendi2@hotmail.com>
2025-06-24 23:31:02 -0500

    implement signals to dynamic-csv-table

--- src/app/shared/components/dynamic-csv-table/dynamic-csv-table.ts
+++ src/app/shared/components/dynamic-csv-table/dynamic-csv-table.ts
@@ -1,5 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, signal, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  signal,
+  SimpleChanges,
+  WritableSignal,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ParseData } from '@shared/utils/parse-data';
 
@@ -13,16 +19,16 @@ export class DynamicCsvTable {
   @Input() dataBase64: string = '';
   @Input() title: string = '';
 
-  csvHeaders: string[] = [];
-  csvData: Record<string, any>[] = [];
-  pagedCsvData: Record<string, any>[] = [];
+  csvHeaders: WritableSignal<string[]> = signal([]);
+  csvData: WritableSignal<Record<string, any>[]> = signal([]);
+  pagedCsvData: WritableSignal<Record<string, any>[]> = signal([]);
+  filteredCsvData: WritableSignal<Record<string, any>[]> = signal([]);
 
   rowsPerPage = 10;
-  currentPage = 1;
-  totalPages = 1;
-  searchTerm: string = '';
-  filteredCsvData: Record<string, any>[] = [];
-  showTable = false;
+  currentPage = signal(1);
+  totalPages = signal(1);
+  searchTerm = signal('');
+  showTable = signal(false);
 
   ngOnInit() {
     this.checkData();
@@ -30,59 +36,65 @@ export class DynamicCsvTable {
 
   checkData() {
     if (this.dataBase64 != '') {
-      this.csvData = ParseData.parseCSV(this.dataBase64);
-      console.log('csv Json', this.csvData);
+      this.csvData.set(ParseData.parseCSV(this.dataBase64));
       this.loadCsvData();
-      this.showTable = true;
+      this.showTable.set(true);
     }
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['dataBase64']) {
+      this.searchTerm.set('');
       this.checkData();
     }
   }
 
   loadCsvData() {
-    this.csvHeaders = Object.keys(this.csvData[0] || {});
-    this.filteredCsvData = this.csvData;
-    this.totalPages = Math.ceil(this.csvData.length / this.rowsPerPage);
-    this.pagedCsvData = this.csvData.slice(0, this.rowsPerPage);
+    this.csvHeaders.set(Object.keys(this.csvData()[0] || {}));
+    this.filteredCsvData.set(this.csvData());
+    this.totalPages.set(Math.ceil(this.csvData().length / this.rowsPerPage));
+    this.pagedCsvData.set(this.csvData().slice(0, this.rowsPerPage));
     this.updatePagedData();
   }
 
   updatePagedData() {
-    const data = this.searchTerm ? this.filteredCsvData : this.csvData;
-    const start = (this.currentPage - 1) * this.rowsPerPage;
+    const data = this.searchTerm() ? this.filteredCsvData() : this.csvData();
+    const start = (this.currentPage() - 1) * this.rowsPerPage;
     const end = start + this.rowsPerPage;
-    this.pagedCsvData = data.slice(start, end);
+    this.pagedCsvData.set(data.slice(start, end));
   }
 
   previousPage() {
-    if (this.currentPage > 1) {
-      this.currentPage--;
+    if (this.currentPage() > 1) {
+      this.currentPage.update((v) => v - 1);
       this.updatePagedData();
     }
   }
 
   nextPage() {
-    if (this.currentPage < this.totalPages) {
-      this.currentPage++;
+    if (this.currentPage() < this.totalPages()) {
+      this.currentPage.update((v) => v + 1);
       this.updatePagedData();
     }
   }
 
-  onSearchChange() {
-    const term = this.searchTerm.toLowerCase();
+  onSearchChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.searchTerm.set(input.value);
+    const term = this.searchTerm().toLowerCase();
 
-    this.filteredCsvData = this.csvData.filter((row) =>
-      Object.values(row).some((value) =>
-        String(value).toLowerCase().includes(term)
+    this.filteredCsvData.set(
+      this.csvData().filter((row) =>
+        Object.values(row).some((value) =>
+          String(value).toLowerCase().includes(term)
+        )
       )
     );
 
-    this.totalPages = Math.ceil(this.filteredCsvData.length / this.rowsPerPage);
-    this.currentPage = 1;
+    this.totalPages.set(
+      Math.ceil(this.filteredCsvData().length / this.rowsPerPage)
+    );
+    this.currentPage.set(1);
     this.updatePagedData();
   }
 }
