2d789c6501ad8d3e9a4e6561fc49a03ebc532ef3
Duvan Caicedo <duvamendi2@hotmail.com>
2025-06-24 23:26:37 -0500

    implement guards on the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,16 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Summary } from './pages/summary/summary';
 import { Layout } from './layout/layout';
+import { sessionFormGuard } from '@core/guards/session-form-guard';
+import { summaryGuard } from '@core/guards/summary-guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: Layout,
     children: [
-      { path: '', component: Home },
-      { path: 'summary', component: Summary },
+      { path: '', component: Home, canActivate: [sessionFormGuard] },
+      { path: 'summary', component: Summary, canActivate: [summaryGuard] },
     ],
   },
   { path: '**', redirectTo: '' },
